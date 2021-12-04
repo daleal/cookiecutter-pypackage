e95673f5bda6400d7f01b0583d1800ad4c7db20d
Daniel Leal <dlleal@uc.cl>
2021-12-04 18:54:16 -0300

    Catch console errors

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -9,61 +9,69 @@ import os
 MANIFEST_FILE = "manifest.json"
 
 def cleanup_disabled_features():
-    print("Cleaning up...")
+    print("⚒ Cleaning up...")
     with open(MANIFEST_FILE) as manifest_file:
         manifest = json.load(manifest_file)
     for feature in manifest["features"]:
         if not feature["enabled"]:
-            print(f"Removing resources for disabled feature {feature['name']}...")
             for resource in feature["resources"]:
                 remove_resource(resource)
     remove_resource(MANIFEST_FILE)
-    print("Cleanup complete")
+    print("✔ Cleanup complete")
 
 
 def remove_resource(resource):
     if os.path.isfile(resource):
-        print(f"Removing file: {resource}")
         os.remove(resource)
     elif os.path.isdir(resource):
-        print(f"Removing directory: {resource}")
         shutil.rmtree(resource)
 
 
 # ENVIRONMENT SETUP
 
 def build_environment():
-    print("Building the environment...")
-    create_virtual_environment()
-    install_dependencies()
-    print("The environment is ready")
+    print("⚒ Building the environment...")
+    try:
+        create_virtual_environment()
+        install_dependencies()
+        print("✔ The environment is ready")
+    except subprocess.CalledProcessError:
+        print(
+            "❌ The environment could not be built, you can do it later "
+            "using the included command `make build-env`."
+        )
 
 
 def create_virtual_environment():
-    subprocess.call(["python3", "-m", "venv", ".venv"])
+    subprocess.check_call(["python3", "-m", "venv", ".venv"])
 
 
 def install_dependencies():
-    subprocess.call(["poetry", "run", "pip", "install", "--upgrade", "pip"])
-    subprocess.call(["poetry", "run", "poetry", "install"])
+    subprocess.check_call(
+        ["poetry", "run", "pip", "install", "--upgrade", "pip"]
+    )
+    subprocess.check_call(["poetry", "run", "poetry", "install"])
 
 
 # GIT INITIALIZATION
 
 def execute_git_initialization():
-    print("Initializing git repository...")
-    initialize_git_repository()
-    initial_git_commit()
-    print("git initialization complete")
+    print("⚒ Initializing git repository...")
+    try:
+        initialize_git_repository()
+        initial_git_commit()
+        print("✔ git initialization complete")
+    except subprocess.CalledProcessError:
+        print("❌ The git repository could not be correctly initialized.")
 
 
 def initialize_git_repository():
-    subprocess.call(["git", "init"])
+    subprocess.check_call(["git", "init"])
 
 
 def initial_git_commit():
-    subprocess.call(["git", "add", "."])
-    subprocess.call(["git", "commit", "-m", "'Initial commit'"])
+    subprocess.check_call(["git", "add", "."])
+    subprocess.check_call(["git", "commit", "-m", "'Initial commit'"])
 
 
 if __name__ == "__main__":
